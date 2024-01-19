fdc744bf988a83859e4e79c65a881734e37ab872
Alexandre Marques <alexandreacm.marques@gmail.com>
2024-01-19 13:40:35 -0300

    feat(navigation, default themes): navigation
    Added some defaults themes and fonts, types.

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  ui: {
+    primary: '#161621',
+    secondary: '#757575',
+    card: '#272639',
+    matchTime: '#FF0000',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#161621',
+  },
+  text: {
+    primary: '#161621',
+    white: '#FFFFFF',
+  },
+};
