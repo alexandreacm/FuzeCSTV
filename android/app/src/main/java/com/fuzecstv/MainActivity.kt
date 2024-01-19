c59f20835d4bdf53edfba9322b42db378e86975e
Alexandre Marques <alexandreacm.marques@gmail.com>
2024-01-19 10:56:18 -0300

    Added some configurations, implemented a splashand icon.

--- android/app/src/main/java/com/fuzecstv/MainActivity.kt
+++ android/app/src/main/java/com/fuzecstv/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.fuzecstv
-
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,10 +8,11 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState)
+  }
+
   override fun getMainComponentName(): String = "FuzeCSTV"
 
   /**
