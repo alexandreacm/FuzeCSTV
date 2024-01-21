c767f167220183275adb2020e119f9eac7b7843c
Alexandre Marques <alexandreacm.marques@gmail.com>
2024-01-20 22:05:38 -0300

    test(jest config): jest config file
    Added some configurations for jest file.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./__tests__/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.(spec|test)'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'node', 'json'],
+  testPathIgnorePatterns: [
+    './node_modules',
+    './android',
+    './ios',
+    '<rootDir>/__tests__/jest.setup.js',
+  ],
 };
